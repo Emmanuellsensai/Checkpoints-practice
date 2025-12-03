df1dcfbcca6ab86c0d473fbf01a0b1b3bdf75dac
nomuekwu <ngozi.omuekwupgs@stu.cu.edu.ng>
2025-12-03 16:51:14 +0100

    modified

---
+++ printrevcomb/printrevcomb.go
@@ -0,0 +1,32 @@
+package piscine
+
+package main
+
+import "github.com/01-edu/z01"
+
+func PrintRevComb() {
+	for a := 9; a >= 0; a-- {
+		for b := a - 1; b >= 0; b-- {
+			for c := b - 1; c >= 0; c-- {
+
+				// Print the 3 digits
+				z01.PrintRune(rune(a + '0'))
+				z01.PrintRune(rune(b + '0'))
+				z01.PrintRune(rune(c + '0'))
+
+				// If this is NOT the last combination, print ", "
+				if !(a == 2 && b == 1 && c == 0) {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+			}
+		}
+	}
+
+	// Final newline
+	z01.PrintRune('\n')
+}
+
+func main() {
+	PrintRevComb()
+}
