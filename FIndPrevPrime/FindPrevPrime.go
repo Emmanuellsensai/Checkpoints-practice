f02be348e36f5dcf8e98fe4232d506fe463b4426
Emmanuell sensai <emmanuelusang60@gmail.com>
2025-12-03 13:28:22 +0100

    Update FindPrevPrime.go

--- FIndPrevPrime/FindPrevPrime.go
+++ FIndPrevPrime/FindPrevPrime.go
@@ -1,20 +1,28 @@
 package piscine
 
 func FindPrevPrime(nb int) int {
+	// 1. Edge Case: Numbers less than 2 cannot be prime.
 	if nb < 2 {
 		return 0
 	}
 
+	// 2. Loop Backwards from 'nb' down to 2.
+	// We want the highest prime less than or equal to 'nb'.
 	for i := nb; i >= 2; i-- {
+		
+		// Assume 'i' is prime until we find a divisor.
 		isPrime := true
 
+		// 3. Inner Loop: Check for divisors from 2 up to the square root of 'i'.
 		for j := 2; j*j <= i; j++ {
 			if i%j == 0 {
-
+				// Divisor found, 'i' is not prime.
 				isPrime = false
-				break 
+				break
 			}
 		}
+
+		// 4. If isPrime is still true, we found our number.
 		if isPrime {
 			return i
 		}
